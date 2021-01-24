983be86cad4a5d75471a2c913e5b22e1033d20a8
scc <15720623130@163.com>
2021-01-24 20:55:53 +0800

    init

--- Demo08.c
+++ Demo08.c
@@ -2,11 +2,15 @@
 #include <stdlib.h>
 #include <time.h>
 
+#define TOATOAL 1024;
+
 typedef unsigned char BYTE;
 
-typedef char * STRING;
+typedef char *STRING;
 
 typedef int array_5[5];
+
+void show_string(STRING);
 
 union hold
 {
@@ -36,26 +40,36 @@ int main(int argc, char *argv[])
 	FILE *fp;
 	union hold element = {.b = 'a'};
 	TIME time;
-	STRING k = "aaaa";
-	printf("%s\n",k);
+	char temp_char_arr[] = {'1','1','\0'};
+	STRING k = temp_char_arr;
+	printf("%s\n", k);
 	enum color color = red;
 	BYTE charele = '1';
 	time = (TIME){
 		1, 2, 3};
 	enum color color1 = green;
 	printf("%d\n", time.hour);
-	printf("%c\n",charele);
+	printf("%c\n", charele);
 	fp = fopen("b.txt", "a+");
 	if (fp == NULL)
 	{
-		fputs("Error opening a.txt file",stderr);
+		fputs("Error opening a.txt file", stderr);
 		exit(1);
 	}
-	fputs("Error opening a.txt file",stderr);
+	fputs("Error opening a.txt file", stderr);
 	rewind(fp);
-	fputs("sunchenchen",fp);
+	fputs("sunchenchen", fp);
 
-	array_5 dogs = {1,2,3,4,5};
-	printf("%d\n",dogs[1]);
+	array_5 dogs = {1, 2, 3, 4, 5};
+	printf("%d\n", dogs[1]);
+
+	void (*p_show)(STRING);
+	p_show = show_string;
+
 	return 0;
+}
+
+void show_string(STRING string)
+{
+	puts(string);
 }
