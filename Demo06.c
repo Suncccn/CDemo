596b3685e1ed5f62caebfa74248c1d46eff79315
scc <15720623130@163.com>
2021-02-01 09:34:28 +0800

    Demo10

--- Demo06.c
+++ Demo06.c
@@ -1,61 +1,23 @@
 #include <stdio.h>
-#include <stdlib.h>
-#define CNTL_Z '\032'
+#include <stdalign.h>
 
-int main(int argc, char *argv[])
+#define MAX 10
+const int MIN = 0;
+#define SIX 2 * 3
+#define SIX 2 * 3
+
+#define output(x) printf(" " #x " %d \n", x)
+#define PR(x, ...) printf("msg " #x ":"__VA_ARGS__)
+
+int main()
 {
-	FILE *fp;
-	long int count;
-	char ch;
-
-	if (argc < 2) 
-	{
-		printf("lack params");
-		exit(EXIT_FAILURE);
-	}
-
-	if (argv[1] == NULL) 
-	{
-		printf("lack file directory");
-		exit(EXIT_FAILURE);
-	}
-
-	fp = fopen(argv[1], "rb");
-
-	if (fp == NULL) 
-	{
-		printf("open file failed");
-		exit(EXIT_FAILURE);
-	}
-	
-	
-	fseek(fp, 0L, SEEK_END);
-	printf("%ld\n", ftell(fp));
-	fseek(fp, -1, SEEK_CUR);
-	putchar(getc(fp));
-	fseek(fp, 0L, SEEK_SET);
-	printf("\n");
-	printf("%ld\n", ftell(fp));
-	putchar(getc(fp));
-
-	count = ftell(fp);
-
-	// printf("%ld\n",count);
-
-	// for (long i = 1l; i <= count; i++)
-	// {
-	// 	fseek(fp, -i, SEEK_SET);
-	// 	ch = getc(fp);
-	// 	printf("%d\n",ch);
-	// 	if (ch != CNTL_Z || ch != '\n')
-	// 	{
-	// 		putchar(ch);
-	// 	}
-
-	// }
-	printf("\n");
-
-	fclose(fp);
-
+	const int a = MAX;
+	const int b = MIN;
+	int six = SIX;
+	printf("%d %d \n", a, b);
+	printf("%d\n", six);
+	output(a);
+	output(10);
+	PR(a, "= %d \n", a);
 	return 0;
 }
--- Demo10.c
+++ Demo10.c
@@ -1,22 +1,29 @@
 #include <stdio.h>
-#include <stdbool.h>
+#include <stdlib.h>
 
-typedef char *string;
-typedef char *dialog[10];
-#define MAX(A,B) (A>B?A:B)
 
-struct list
-{
-	unsigned int i:1;
-	unsigned int j:2;
-	unsigned int k:32;
-};
 int main()
 {
-	string str;
-	str = "sunchenchen";
-	printf("%010d\n", 1919);
-	printf("%s\n", str);
-	printf("%lu\n",sizeof(struct list));
-	return 0;
+    // char name1[4][10] = { "Justinian", "Momo", "Becky", "Bush" };
+    // char *name2[4] = { "Justinian", "Momo", "Becky", "Bush" };
+
+    // printf("--------二维字符串数组的存储方式-------\n");
+    // for(int i = 0; i < 4; i++)
+    // {
+    //     printf("name[%d] = \"%s\"\t", i, name1[i]);
+    //     printf("所占地址：%p\n", name1[i]);
+    // }
+
+    // printf("--------一维指针数组的存储方式--------\n");
+    // for(int i = 0; i < 4; i++)
+    // {
+    //     printf("name[%d] = \"%s\"\t", i, name2[i]);
+    //     printf("所占地址：%p\n", name2[i]);
+    // }
+
+	char *arr[1];
+	char *p = (char *)malloc(sizeof(char)*100);
+	scanf("%s",p);
+	arr[0] = p;
+	printf("%s",arr[0]);
 }
