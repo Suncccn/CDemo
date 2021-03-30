5d4ec6878dd796974d8e181b0f2b4a2904822e9e
scc <15720623130@163.com>
2021-03-30 17:13:27 +0800

    Cdemo

--- Demo15.c
+++ Demo15.c
@@ -1,11 +1,42 @@
 #include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <fcntl.h>
 #include <stdlib.h>
-#include <sys/socket.h>
 
-#define sin(X) _Generic((X),\
-	float:float,\
-	double:double)
+int main()
+{
+	pid_t pid;
+	int fd[2];
+	if (pipe(fd) == -1)
+	{
+		perror("Error: create pipe failed\n");
+		exit(1);
+	}
 
-int main(){
-	
+	open("./sort.c",O_RDWR);
+	if ((pid = fork()) < 0)
+	{
+		printf("Error:create process error\n");
+		exit(1);
+	}
+	else if (pid == 0)
+	{
+		close(fd[0]);
+		printf("child process write success\n");
+		write(fd[1], "hello world parent!", 20);
+		_exit(0);
+	}
+	else
+	{
+		close(fd[1]);
+		printf("parent receive:");
+		char message[1024];
+		read(fd[0],message,1024);
+		printf("%s\n",message);
+		wait(NULL);
+		_exit(0);
+	}
+	return 0;
 }
---
+++ Demo18.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <netdb.h>
+#include <sys/socket.h>
+#include <sys/utsname.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+
+
+int main(int argc, char *argv[])
+{
+	// struct utsname uts;
+	// uname(&uts);
+	// printf("%s\n%s\n", uts.sysname,uts.nodename);
+	// struct in_addr addr;
+	// printf("%s\n", strerror(errno));
+	
+	return 0;
+}
